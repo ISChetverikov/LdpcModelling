1bade651a62e0ae83d90b7989c1f3b6e09550ccb
Gorogorov <hpaapipny@yandex.ru>
2020-04-05 02:43:02 +0300

    Q

--- LDPC_cpp/src/SP_decoder.cpp
+++ LDPC_cpp/src/SP_decoder.cpp
@@ -1,8 +1,11 @@
-
+#include <vector>
+#include <map>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <algorithm>
 
 #include "../include/SP_decoder.h"
-#include "../include/Exceptions.h"
-#include "../include/MathOperations.h"
 
 #include <vector>
 #include <map>
@@ -11,142 +14,121 @@
 #include <iostream>
 #include <algorithm>
 
-using namespace std;
+int spSign(double x) {
+	return (x < 0) ? 1 : 0;
+}
 
 static float phi(float x)
 {
-    static const float lim = 31.;
-    static const float inv_lim =
-    log( (exp((double)lim) + 1)/(exp((double)lim) - 1) );
-    
-    if (x > lim)
-    {
-        return( 0 );
-    }
-    else if (x < inv_lim)
-        return( lim );
-    else
-    {
-        double t = exp( (double) x );
-        return (float) log( (t+1)/(t-1) );
-    }
+	static const float lim = 31.;
+	static const float inv_lim = 
+		log( (exp((double)lim) + 1)/(exp((double)lim) - 1) );
+	
+	if (x > lim)
+	{
+		return( 0 );
+	}
+	else if (x < inv_lim)
+		return( lim );
+	else 
+	{
+		double t = exp( (double) x );
+		return (float) log( (t+1)/(t-1) );
+	}
 }
 
 // Constructor
-SP_decoder::SP_decoder(vector<vector<int>> H, int iterationsCount) : Base_decoder(H, iterationsCount) {
-    
+SP_decoder::SP_decoder(std::vector<std::vector<int>> H, int iterationsCount) 
+                 : Base_decoder(H, iterationsCount) { }
+
+
+void SP_decoder::HorizontalStep(std::vector<std::map<int, int>> alpha, std::vector<std::map<int, double>> beta, std::vector<std::map<int, double>> &gamma) {
+	int sign = 0;
+	for (size_t j = 0; j < _m; j++)
+	{
+		int sum_sign = 0; // May be with count of sign it will be faster?
+		double values = 0;
+		for (auto &i : _checks[j])
+		{
+			sum_sign ^= alpha[j][i];
+			values += beta[j][i];
+		}
+		for (auto &i : _checks[j])
+		{
+			sign = sum_sign ^ alpha[j][i];
+			gamma[j][i] = (1 - 2*sign) * phi(values - (beta[j][i]));
+		}
+	}
 }
 
-void SP_decoder::HorizontalStep(vector<map<int, int>> alpha, vector<map<int, double>> beta, vector<map<int, double>> &gamma) {
-    int sign = 0;
-    for (size_t j = 0; j < _m; j++)
-    {
-        int sum_sign = 1; // May be with count of sign it will be faster?
-        double values = 0;
-        for (auto &i : _checks[j])
-        {
-            sum_sign ^= alpha[j][i];
-            values += phi(beta[j][i]);
-            // TODO: Here we can get rid of redundant cycle ?
-        }
-        for (auto &i : _checks[j])
-        {
-            sign = sum_sign ^ alpha[j][i];
-            gamma[j][i] = sign * phi(values - phi(beta[j][i]));
-        }
-    }
-}
+std::vector<int> SP_decoder::Decode(std::vector<double> llr, bool *isFailed) {
+	size_t n = llr.size();
+	// if (n != _n)
+	// 	throw IncorrectCodewordException("The codeword is not from a code with given check matrix");
 
-vector<int> SP_decoder::Decode(vector<double> llr, bool *isFailed) {
-    
-    size_t n = llr.size();
-    // if (n != _n)
-    //     throw IncorrectCodewordException("The codeword is not from a code with given check matrix");
-    
-    vector<int> result(n, 0);
-    
-    //vector<int> alpha0(n, 0);
-    //vector<double> beta0(n, 0);
-    vector<double> bits_values(n, 0);
-    //for (size_t i = 0; i < n; i++)
-    //{
-    //    alpha0[i] = sign(llr[i]);
-    //    beta0[i] = fabs(llr[i]);
-    //}
-    
-    vector<map<int, int>> alpha(_m, map<int, int>());
-    vector<map<int, double>> beta(_m, map<int, double>());
-    vector<map<int, double>> gamma(_m, map<int, double>());
-    
-    // Init
-    for (size_t j = 0; j < _m; j++)
-    {
-        for (auto &i : _checks[j])
-        {
-            alpha[j][i] = sign(llr[i]);
-            beta[j][i] = fabs(llr[i]);
-        }
-    }
-    
-    size_t iteration = 0;
-    
-    while (true)
-    {
-        iteration++;
-        HorizontalStep(alpha, beta, gamma);
-        
-        // Result of iteration
-        for (size_t i = 0; i < n; i++)
-        {
-            bits_values[i] = llr[i];
-            
-            for (auto &j : _bits[i])
-            {
-                bits_values[i] += gamma[j][i];
-            }
-            
-            //alpha0[i] = sign(bits_values[i]);
-            //beta0[i] = fabs(bits_values[i]);
-        }
-        
-        for (size_t i = 0; i < n; i++)
-        {
-            result[i] = (bits_values[i] <= 0) ? 1 : 0;
-        }
-        
-        //if (_H * result == vector<int>(_m, 0))
-        //    break;
-        for (size_t j = 0; j < _m; j++)
+	std::vector<int> result(n, 0);
+	std::vector<double> bits_values(n, 0);
+	std::vector<std::map<int, int>> alpha(_m, std::map<int, int>());
+	std::vector<std::map<int, double>> beta(_m, std::map<int, double>());
+	std::vector<std::map<int, double>> gamma(_m, std::map<int, double>());
+
+	// Init
+	for (size_t j = 0; j < _m; j++)
+	{
+		for (auto &i : _checks[j])
+		{
+			alpha[j][i] = spSign(llr[i]);
+			beta[j][i] = phi(fabs(llr[i]));
+		}
+	}
+	size_t iteration = 0;
+
+	while (iteration < _iterationsCount)
+	{
+		iteration++;
+		HorizontalStep(alpha, beta, gamma);
+
+		 // Result of iteration
+		for (size_t i = 0; i < n; i++)
+		{
+			bits_values[i] = llr[i];
+			for (auto &j : _bits[i])
+			{
+				bits_values[i] += gamma[j][i];
+			}
+		}
+
+		for (size_t i = 0; i < n; i++)
+			result[i] = (bits_values[i] <= 0) ? 1 : 0;
+
+		for (size_t j = 0; j < _m; j++)
         {
             int sum = 0;
             for (auto &i : _checks[j])
             {
                 sum ^= result[i];
             }
-            if ((bool)sum) {
+            /*if ((bool)sum) {
                 *isFailed = true;
                 break;
-            }
+            }*/
         }
-        
-        
-        if (iteration >= _iterationsCount)
-            break;
-        
-        // Vertical Step
-        for (size_t i = 0; i < n; i++)
-        {
-            double value = bits_values[i];
-            
-            for (auto &j : _bits[i])
-            {
-                double new_value = value - gamma[j][i];
-                alpha[j][i] = sign(new_value);
-                beta[j][i] = fabs(new_value);
-            }
-        }
-    }
-    
-    return result;
-}
 
+
+		if (iteration >= _iterationsCount)
+			break;
+
+		//Vertical Step
+		for (size_t i = 0; i < n; i++)
+		{
+			double value = bits_values[i];
+			for (auto &j : _bits[i])
+			{
+				double new_value = value - gamma[j][i];
+				alpha[j][i] = spSign(new_value);
+				beta[j][i] = phi(fabs(new_value));
+			}
+		}
+	}
+	return (result);
+}
