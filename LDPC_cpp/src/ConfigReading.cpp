8640494bf0787dae3c06550214109f5e7ad2b358
Ilya <ischetverikov@gmail.com>
2020-04-11 21:07:25 +1000

    corrections

--- LDPC_cpp/src/ConfigReading.cpp
+++ LDPC_cpp/src/ConfigReading.cpp
@@ -24,7 +24,7 @@ pugi::xml_node TryGetChildNode(pugi::xml_node node, std::string childName) {
 	return child;
 }
 
-pugi::xml_attribute TryGetAttribute(pugi::xml_node node, std::string name) {
+pugi::xml_attribute GetAttribute(pugi::xml_node node, std::string name) {
 	auto attr = node.attribute(name.c_str());
 	if (attr == NULL)
 		throw ConfigParseException("Parse config error: can not find \"" + name + "\" atribute");
@@ -40,7 +40,7 @@ SimulationParams ReadSimulationSection(pugi::xml_node simulation_node) {
 
 	pugi::xml_node simulator_node = GetChildNode(simulation_node, "Simulator");
 
-	std::string simulationTypeStr = TryGetAttribute(simulator_node, "type").value();
+	std::string simulationTypeStr = GetAttribute(simulator_node, "type").value();
 	simulationType simulationType_ = simulationTypeFromString(simulationTypeStr);
 	if (simulationType_ == simulationType::UnknownSimulation)
 		throw ConfigParseException("Parse config error: unsupported value of simulation type: " + simulationTypeStr);
@@ -50,13 +50,13 @@ SimulationParams ReadSimulationSection(pugi::xml_node simulation_node) {
 	std::unordered_map <std::string, std::string > simulationParams;
 	for (pugi::xml_node simulationParam_node : simulator_node.children("SimulationParam"))
 	{
-		simulationParams[TryGetAttribute(simulationParam_node, "name").value()] = simulationParam_node.child_value();
+		simulationParams[GetAttribute(simulationParam_node, "name").value()] = simulationParam_node.child_value();
 	}
 	params.simulationTypeParams = simulationParams;
 
 	pugi::xml_node decoder_node = GetChildNode(simulation_node, "Decoder");
 
-	std::string decoderTypeStr = TryGetAttribute(decoder_node, "type").value();
+	std::string decoderTypeStr = GetAttribute(decoder_node, "type").value();
 	decoderType decoderType_ = decoderTypeFromString(decoderTypeStr);
 	if (decoderType_ == decoderType::UnknownDecoder)
 		throw ConfigParseException("Parse config error: unsupported value of decoder type: " + decoderTypeStr);
@@ -66,16 +66,16 @@ SimulationParams ReadSimulationSection(pugi::xml_node simulation_node) {
 	std::unordered_map <std::string, std::string > decoderParams;
 	for (pugi::xml_node decoderParam_node : decoder_node.children("DecoderParam"))
 	{
-		decoderParams[TryGetAttribute(decoderParam_node, "name").value()] = decoderParam_node.child_value();
+		decoderParams[GetAttribute(decoderParam_node, "name").value()] = decoderParam_node.child_value();
 	}
 	params.decoderParams = decoderParams;
 
 	pugi::xml_node snrRange_node = TryGetChildNode(simulation_node, "SnrRange");
 	bool isSnrPresent = false;
 	if (snrRange_node != NULL) {
-		for (double i = std::stod(TryGetAttribute(snrRange_node, "start").value());
-			i < std::stod(TryGetAttribute(snrRange_node, "stop").value());
-			i += std::stod(TryGetAttribute(snrRange_node, "step").value()))
+		for (double i = std::stod(GetAttribute(snrRange_node, "start").value());
+			i < std::stod(GetAttribute(snrRange_node, "stop").value());
+			i += std::stod(GetAttribute(snrRange_node, "step").value()))
 			params.snrArray.push_back(i);
 		isSnrPresent = true;
 	}
